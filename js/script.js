2c434990fb2912d1d66376c4f10b11275e2bf538
Lucas Nadaes <lucasnadaes@outlook.com>
2022-10-27 21:34:14 -0300

    Inclusão de animação Index

--- js/script.js
+++ js/script.js
@@ -47,3 +47,28 @@ function eventosPerguntas(pergunta) {
 perguntas.forEach(eventosPerguntas)
 
 console.log(perguntas);
+
+//Galeria de bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria)
+
+//Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
